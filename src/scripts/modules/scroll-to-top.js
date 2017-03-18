daec7a2fbc887b8329dfaafdad1e8f46a4108442
Malin Antonsson <malin@malinantonsson.com>
2017-03-18 16:40:26 +0000

    add icon to scroll to top

--- src/scripts/modules/scroll-to-top.js
+++ src/scripts/modules/scroll-to-top.js
@@ -3,7 +3,7 @@ function scrollToTop () {
     const SCROLL_TO_TOP = document.querySelector('#scroll-to-top');
 
     if(SCROLL_TO_TOP) {
-        let markup = `<span class="scroll-to-top__icon">Icon</span>
+        let markup = `<span class="scroll-to-top__icon"></span>
         {<span class="scroll-to-top__copy"> Back to Top </span>}`;
         SCROLL_TO_TOP.innerHTML = markup;
     }
