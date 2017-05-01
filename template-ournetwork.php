a6017a36b29727a666ac3f2b0b2a455820491aaf
Malin Antonsson <malin@malinantonsson.com>
2017-05-01 15:53:08 +0100

    Update twitter feed brackets

--- template-ournetwork.php
+++ template-ournetwork.php
@@ -7,77 +7,35 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<div id="content" class="site-content" role="main">
-	Back to our network
+
+	
 	<?php while ( have_posts() ) : the_post(); ?>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
-
-				<div class="entry-main">
-
-					<?php do_action('vantage_entry_main_top') ?>
-
-					<?php if ( ( the_title( '', '', false ) && siteorigin_page_setting( 'page_title' ) ) || ( has_post_thumbnail() && siteorigin_setting('blog_featured_image') ) || ( siteorigin_setting( 'blog_post_metadata' ) && get_post_type() == 'post' ) ) : ?>
-						<header class="entry-header">
-
-							<?php if( has_post_thumbnail() && siteorigin_setting('blog_featured_image') ): ?>
-								<div class="entry-thumbnail"><?php vantage_entry_thumbnail(); ?></div>
-							<?php endif; ?>
-
-							<?php if ( the_title( '', '', false ) && siteorigin_page_setting( 'page_title' ) ) : ?>
-								<h1 class="entry-title"><?php the_title(); ?></h1>
-							<?php endif; ?>
-
-							<?php if ( siteorigin_setting( 'blog_post_metadata' ) && get_post_type() == 'post' ) : ?>
-								<div class="entry-meta">
-									<?php vantage_posted_on(); ?>
-								</div><!-- .entry-meta -->
-							<?php endif; ?>
-
-						</header><!-- .entry-header -->
-					<?php endif; ?>
-
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'vantage' ), 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->
-
-					<?php if( vantage_get_post_categories() && ! is_singular( 'jetpack-testimonial' ) ) : ?>
-						<div class="entry-categories">
-							<?php echo vantage_get_post_categories() ?>
-						</div>
-					<?php endif; ?>
-
-					<?php if( is_singular() && siteorigin_setting( 'blog_author_box' ) ) : ?>
-						<div class="author-box">
-							<div class="avatar-box">
-								<div class="avatar-wrapper"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 70 ) ?></div>
-							</div>
-							<div class="box-content entry-content">
-								<h3 class="box-title"><?php echo esc_html( get_the_author_meta( 'display_name' ) ) ?></h3>
-								<div class="box-description">
-									<?php if( get_the_author_meta( 'description' ) ) : ?>
-										<?php echo wp_kses_post( wpautop( get_the_author_meta( 'description' ) ) ) ?>
-									<?php elseif( current_user_can( 'edit_users', $post->post_author ) ) : ?>
-										<a href="<?php echo get_edit_user_link( $post->post_author ); ?>"><?php _e( 'Add author biographical info.', 'vantage' ) ?></a>
-									<?php endif; ?>
-								</div>
-							</div>
-						</div>
-					<?php endif; ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 
 
-					<?php do_action('vantage_entry_main_bottom') ?>
+			<div class="entry-main">
+			<a href="/our-network/">Our network</a>
 
-				</div>
+				<?php do_action('vantage_entry_main_top') ?>
 
-			</article><!-- #post-<?php the_ID(); ?> -->
-			
+				<?php if ( ( the_title( '', '', false ) && siteorigin_page_setting( 'page_title' ) ) || ( has_post_thumbnail() && siteorigin_setting('blog_featured_image') ) || ( siteorigin_setting( 'blog_post_metadata' ) && get_post_type() == 'post' ) ) : ?>
+					<header class="entry-header">
 
-		<?php if ( siteorigin_setting( 'navigation_post_nav' ) ) vantage_content_nav( 'nav-below' ); ?>
+						<?php if ( the_title( '', '', false ) && siteorigin_page_setting( 'page_title' ) ) : ?>
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+						<?php endif; ?>
 
-		<?php if ( comments_open() || '0' != get_comments_number() ) : ?>
-			<?php comments_template( '', true ); ?>
-		<?php endif; ?>
+					</header><!-- .entry-header -->
+				<?php endif; ?>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
+
+			</div>
+
+		</article><!-- #post-<?php the_ID(); ?> -->
 
 	<?php endwhile; // end of the loop. ?>
 
